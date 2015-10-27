df33a3a82957cc8e0c30498c4003cae0e521bb03
Stian Larsen (lonix) <lonixx@gmail.com>
2015-10-27 13:10:17 +0100

    cp

--- init/30_symlink.sh
+++ init/30_symlink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /config/{log,lib}
-
+[[ ! -d /config/lib ]] && cp -pr /var/lib/unifi.org /config/lib
 [[ ! -L /var/log/unifi ]] && ln -s /config/log /var/log/unifi
 [[ ! -L /var/lib/unifi ]] && ln -s /config/lib /var/lib/unifi
 
