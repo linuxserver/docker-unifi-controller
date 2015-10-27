f3c692ea88293b77b5b7f532013099617948ecd0
Stian Larsen (lonix) <lonixx@gmail.com>
2015-10-27 14:15:08 +0100

    //

--- init/30_symlink.sh
+++ init/30_symlink.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p /config/{log,lib}
-[[ ! -L /var/log/unifi ]] && ln -s /config/log /var/log/unifi
-[[ ! -L /var/lib/unifi ]] && ln -s /config/lib /var/lib/unifi
+[[ ! -L /var/log/unifi ]] && ln -s /config/log /var/log/unifi/
+[[ ! -L /var/lib/unifi ]] && ln -s /config/lib /var/lib/unifi/
 
 
 
