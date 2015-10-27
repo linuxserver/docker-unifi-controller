086882b2fc1d7a07c9cdfbaa6fc870d91f19d4fd
Stian Larsen (lonix) <lonixx@gmail.com>
2015-10-27 12:53:38 +0100

    Added symlink

---
+++ init/30_symlink.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+mkdir -p /config/{log,lib}
+[[ -L /var/log/unifi ]] && ln -s /config/log /var/log/unifi
+[[ -L /var/lib/unifi ]] && ln -s /config/lib /var/lib/unifi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
